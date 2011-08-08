588afe41b08df6aa5e8f2f2d92431ad600ecc7db
Pavel Labath <pavelo@centrum.sk>
2011-08-08 13:00:13 +0200

    Don't create a new lua state upon reconfigure
    As we integrate lua more and more into the core of fluxbox, it's going to progressively harder to
    update all things that reference to the old state.
    Furthermore, I would like to completely drop the reconfiguration at some point, after I'm
    finished with making sure that all resources use the modifiedSig signal to update automatically.

--- src/fluxbox.cc
+++ src/fluxbox.cc
@@ -1233,9 +1233,6 @@ BScreen *Fluxbox::findScreen(int id) {
 }
 
 void Fluxbox::reconfigure() {
-    std::auto_ptr<FbTk::Lua> t = m_l;
-    m_l.reset(new Lua);
-    m_resourcemanager.setLua(*m_l);
     load_rc();
     STLUtil::forAll(m_screen_list, mem_fun(&BScreen::reconfigure));
 }
